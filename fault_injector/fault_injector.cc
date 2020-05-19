487520ae36336fca38070663a90556d7a8a9c2fe
Oleksii Oleksenko <alexo_o@ukr.net>
2020-05-19 16:12:04 +0200

    lab7: code style refactoring

--- fault_injector/fault_injector.cc
+++ fault_injector/fault_injector.cc
@@ -16,71 +16,65 @@ const double crash_trashhold = 0.025;;
 // if drand48 is not called before the fork, childs starts
 // with the same seed
 //void init () __attribute__ ((constructor));
-static void init ()
-{
+static void init() {
     struct timespec now;
-    
-    clock_gettime (CLOCK_REALTIME, &now);
-    seed48((unsigned short*)&now);
+
+    clock_gettime(CLOCK_REALTIME, &now);
+    seed48((unsigned short *) &now);
 }
 
-static bool should_abort ()
-{
-	static bool first = true;
-	if (first)
-	{
-		first = false;
-		init ();
-	}
-	return drand48() < crash_trashhold;
+static bool should_abort() {
+    static bool first = true;
+    if (first) {
+        first = false;
+        init();
+    }
+    return drand48() < crash_trashhold;
 }
 
 #define jump_to(func)                                                                 \
     asm("movl %ebp,%esp");                                                            \
     asm("popl %ebp");                                                                 \
     asm("jmp  *libc_" #func);
-	
+
 #define resolver(func)                                                                \
 static void unresolved_##func();                                                      \
 __typeof(func)* libc_##func = (__typeof(func)*) unresolved_##func;                    \
 static void unresolved_##func()                                                       \
 {                                                                                     \
-	if (!libc_##func || (__typeof(func)*)unresolved_##func == libc_##func)            \
-	{                                                                                 \
-		libc_##func = (__typeof(func)*)dlsym (RTLD_NEXT, #func);                      \
-		if (NULL == libc_##func)                                                      \
-		{                                                                             \
-			perror ("Could find orignal " #func " function");                         \
-			abort ();                                                                 \
-		}                                                                             \
-	}                                                                                 \
-	                                                                                  \
-	jump_to(func)                                                                     \
+    if (!libc_##func || (__typeof(func)*)unresolved_##func == libc_##func)            \
+    {                                                                                 \
+        libc_##func = (__typeof(func)*)dlsym (RTLD_NEXT, #func);                      \
+        if (NULL == libc_##func)                                                      \
+        {                                                                             \
+            perror ("Could find orignal " #func " function");                         \
+            abort ();                                                                 \
+        }                                                                             \
+    }                                                                                 \
+                                                                                      \
+    jump_to(func)                                                                     \
 }
 
 resolver(socket)
-int socket(int domain, int  type, int protocol)
-{
-	if (should_abort ())
-		abort ();
-	
-	jump_to(socket)
+int socket(int domain, int type, int protocol) {
+    if (should_abort())
+        abort();
+
+    jump_to(socket)
 }
 
 resolver(recv)
-ssize_t recv(int s, void *buf, size_t len, int flags)
-{
-	if (should_abort ())
-		abort ();
-		
-	jump_to(recv)
+ssize_t recv(int s, void *buf, size_t len, int flags) {
+    if (should_abort())
+        abort();
+
+    jump_to(recv)
 }
 
 resolver(gethostbyname)
-struct hostent *gethostbyname(const char *name)
-{
-	if (should_abort ())
-		abort ();
-		
-	jump_to(gethostbyname)
+struct hostent *gethostbyname(const char *name) {
+    if (should_abort())
+        abort();
+
+    jump_to(gethostbyname)
 }
